dfc43f807f2421d0b1ef82718e22859d18a3563c
Gino <ginoalodar@gmail.com>
2022-03-27 12:38:29 -0500

    Implemented Cart Layout, still not interactive

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Container, Typography, Button, Grid} from '@material-ui/core';
+
+import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
+
+const Cart = ({cart}) => {
+  const classes = useStyles();
+
+  function EmptyCart() {
+    return (
+      <Typography variant='subtitle1'>No Items In The Cart!</Typography>
+    );
+  }
+
+  const FilledCart = () =>(
+    <>
+      <Grid container spacing={3}>
+        {cart.line_items.map((item)=>(
+          <Grid item xs={12} sm={4} key={item.id}>
+            <CartItem item={item}/>
+          </Grid>
+        ))}
+      </Grid>
+      <div className={classes.cardDetails}>
+          <Typography variant='h4'>
+            Subtotal: {cart.subtotal.fomatted_with_symbol}
+          </Typography>
+          <div>
+            <Button className={classes.emptyButton} type='button' size='large' variant='contained' color='secondary'>Empty</Button>
+            <Button className={classes.checkoutButton} type='button' size='large' variant='contained' color='primary'>Checkout</Button>
+          </div>
+      </div>
+    </>
+  );
+
+  if(!cart.line_items) return 'Loading...';
+
+  return (
+    <div>
+      <Container>
+        <div className={classes.toolbar}/>
+        <Typography className={classes.title} variant='h3' gutterBottom>Shopping Cart</Typography>
+        {!cart.line_items.length ? <EmptyCart/> : <FilledCart/>}
+      </Container>
+    </div>
+  )
+}
+
+export default Cart
